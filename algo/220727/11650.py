17c0afa123743aaa5080d839a1ee28677a1f2652
kimdongjun <cjfal2@kakao.com>
2022-08-12 20:29:15 +0900

    Delete algo directory

--- algo/220727/11650.py
+++
@@ -1,6 +0,0 @@
-N = int(input())
-D = []
-for i in range(N):
-    A,B = map(int,input().split())
-    D.append([A,B])
-print(D)
--- algo/220727/2609.py
+++
@@ -1,8 +0,0 @@
-
-
-
-import math
-import sys
-N,M = map(int,sys.stdin.readline().split())
-print(math.gcd(N,M))
-print(math.lcm(N,M))
