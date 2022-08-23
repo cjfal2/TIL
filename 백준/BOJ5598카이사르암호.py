208427a70487ed7743fae334d061cd17833bec87
cjfal2 <cjfal2@kakao.com>
2022-08-24 07:41:01 +0900

    0824 cm2

--- 백준/BOJ5598카이사르암호.py
+++
@@ -1,7 +0,0 @@
-word = list(map(ord,input()))
-for i in range(len(word)) :
-    if word[i] < 68 :
-        word[i] = word[i] + 23
-    else :    
-        word[i] = word[i] - 3
-print(*list(map(chr,word)),sep='') 
